c37ef1fbc850116a431b79aa22e2cb8ac1254f92
tylerkain <tyler.kain@outlook.com>
2023-03-04 11:40:45 -0600

    refactor: removed shell parameter

--- CyberSecurity-Program/mac_changer.py
+++ CyberSecurity-Program/mac_changer.py
@@ -4,8 +4,7 @@ import subprocess
 
 def change_mac(interface, mac):
     '''MAC Changer Function '''
-    subprocess.call(['ifconfig'], shell=True)
-    subprocess.call(['ifconfig', interface, 'down'], shell=True)
-    subprocess.call(['ifconfig', interface, 'hw', 'ether', mac], shell=True)
     print(f'changing {interface} to {mac}')
-    subprocess.call(['ifconfig', interface, 'up'], shell=True)
+    subprocess.call(['ifconfig', interface, 'down'])
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', mac])
+    subprocess.call(['ifconfig', interface, 'up'])
