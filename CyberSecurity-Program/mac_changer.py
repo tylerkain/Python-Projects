6a85dbae7002d0d62ef22a63b6a2dcd6dcaeed44
tylerkain <tyler.kain@outlook.com>
2023-03-04 14:09:20 -0600

    refactor: added new function and conditionals

--- CyberSecurity-Program/mac_changer.py
+++ CyberSecurity-Program/mac_changer.py
@@ -1,5 +1,21 @@
-'''subprocess module'''
+''' modules'''
 import subprocess
+import optparse
+
+
+def get_input():
+    """Get user input"""
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface",
+                      help="interface to change")
+    parser.add_option("-m", "--mac", dest="new_mac",
+                      help="new mac address for interface")
+    options, args  = parser.parse_args()
+    if not options.interface: 
+        parser.error("[-] Please specify interface, use --help")
+    elif not options.mac: 
+        parser.error("[-] Please specify mac, use --help")
+    return options
 
 
 def change_mac(interface, mac):
@@ -8,3 +24,7 @@ def change_mac(interface, mac):
     subprocess.call(['ifconfig', interface, 'down'])
     subprocess.call(['ifconfig', interface, 'hw', 'ether', mac])
     subprocess.call(['ifconfig', interface, 'up'])
+    return change_mac
+
+options = get_input()
+change_mac(options.interface, options.mac)
--- CyberSecurity-Program/main.py
+++ CyberSecurity-Program/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-from mac_changer import change_mac
+import mac_changer 
 while True:
     user_selection = input("select program you want to run: \n 1: Change_Mac \n 2: Network_Scan \n")
     if user_selection == '1':
-        interface = input('[+] Wireless adapter to change: ')
-        mac = input('[+] New MAC address:  ')
-        change_mac(interface, mac)
+           mac_changer.change_mac(interface=mac_changer.options, mac=mac_changer.options)
