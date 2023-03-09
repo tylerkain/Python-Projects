7d57a779ea2b9bc13f6c2db104579527c31cb119
Tyler Kain <tylerkain@Tylers-MacBook-Pro.local>
2023-03-08 19:42:58 -0600

    feat: added new function

--- CyberSecurity-Program/wifi_scan.py
+++ CyberSecurity-Program/wifi_scan.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 """Modules"""
 import subprocess
+import sys
 
 """variables"""
 adapter = input("[+] Input wifi adapter or wireless card: ")
+
 
 def monitor_mode(interface): 
     '''enable monitor mode '''
@@ -14,8 +16,19 @@ def monitor_mode(interface):
 
 def check_wps(interface): 
     """Check for Wps enabled"""
-    print(f"using {interface} to check for wps")
-    subprocess.call(['wash', '--interface', interface])
+    try:
+        print(f"using {interface} to check for wps")
+        subprocess.call(['wash', '--interface', interface])
+    except KeyboardInterrupt: 
+        sys.quit()
+
+def handshake(adapter, wifi_mac, adapter_mac): 
+    """capture handshake"""
+    print("print capturing handshake")
+    wifi_mac = input("[+] Input wifi mac address: ")
+    adapter_mac = input("[+ Input adapter mac: ")
+    subprocess.call(['aireplay-ng','--fakeauth 30', '-a', wifi_mac, '-h', adapter_mac, adapter])
 
 monitor_mode(adapter)
-check_wps(adapter)   
+check_wps(adapter)
+handshake(adapter)   
