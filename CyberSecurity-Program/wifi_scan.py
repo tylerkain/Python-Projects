6e2163d2c152a222b7df8505b5a9d7c189f5148b
Tyler Kain <tylerkain@Tylers-MacBook-Pro.local>
2023-03-08 19:55:09 -0600

    refactor: adding try and except

--- CyberSecurity-Program/wifi_scan.py
+++ CyberSecurity-Program/wifi_scan.py
@@ -16,11 +16,8 @@ def monitor_mode(interface):
 
 def check_wps(interface): 
     """Check for Wps enabled"""
-    try:
-        print(f"using {interface} to check for wps")
-        subprocess.call(['wash', '--interface', interface])
-    except KeyboardInterrupt: 
-        sys.exit(0)
+    print(f"using {interface} to check for wps")
+    subprocess.call(['wash', '--interface', interface])
 
 def handshake(adapter, wifi_mac, adapter_mac): 
     """capture handshake"""
@@ -28,7 +25,9 @@ def handshake(adapter, wifi_mac, adapter_mac):
     wifi_mac = input("[+] Input wifi mac address: ")
     adapter_mac = input("[+ Input adapter mac: ")
     subprocess.call(['aireplay-ng','--fakeauth 30', '-a', wifi_mac, '-h', adapter_mac, adapter])
-
-monitor_mode(adapter)
-check_wps(adapter)
-handshake(adapter)   
+try:
+    monitor_mode(adapter)
+    check_wps(adapter)
+except KeyboardInterrupt:
+    handshake(adapter)
+       
