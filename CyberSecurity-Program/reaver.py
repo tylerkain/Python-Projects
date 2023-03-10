1381dcfa9595536a4e0ca2d33fe254765233e866
Tyler Kain <tylerkain@Tylers-MacBook-Pro.local>
2023-03-10 11:20:04 -0800

    fix: added comma

--- CyberSecurity-Program/reaver.py
+++ CyberSecurity-Program/reaver.py
@@ -6,4 +6,4 @@ def reaver(adapter):
     channel = input("[+] input channel: ")
     wifi_mac = input("[+] Input wifi mac address: ")
     interface = adapter
-    subprocess.call(['reaver', '--bssid', wifi_mac,'--channel', channel, '--interface', interface '-vvv', '--no-associate'])
+    subprocess.call(['reaver', '--bssid', wifi_mac,'--channel', channel, '--interface', interface, '-vvv', '--no-associate'])
